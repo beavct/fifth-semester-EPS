04d3d3b6a790b22f2a3c1833c0bcf3fd3d1e3b06
Bea <beatrizvianacosta@usp.br>
2024-06-05 16:27:23 -0300

    na teoria criei o sistema de arquivos

--- operating-systems-mac0422/EP3/ep3.c
+++ operating-systems-mac0422/EP3/ep3.c
@@ -1,49 +1,154 @@
-/*Cabeçalho retirado da "man syscall"*/
-#include <sys/syscall.h>
-#include <unistd.h>
 #include <stdio.h>
 #include "ep3.h"
 
+/*Para as funções de shell*/
 #include <readline/readline.h>
 #include <readline/history.h>
 
-#include <sys/utsname.h> /*struct utsname*/
-#include <sys/time.h> /*gettimeofday*/
+#include <time.h> 
 #include <sys/types.h> /*uid_t*/
-#include <pwd.h> /*mexer no arquivo pwd*/
 #include <stdlib.h> /*free*/
 #include <string.h> /*strtok*/
-#include <sys/wait.h> /*waitpid*/
 
-void monta(char* arquivo){
+// arrumar essa função aqui pra printar mais bonito
+void printa_arvore(Arquivo* root, int prof) {
+    if (root == NULL) return;
+    for (int i = 0; i < prof; i++) printf("  ");
+    printf("%s%s\n", root->nome, root->ehDir ? "/" : "");
+    printa_arvore(root->filho, prof + 1);
+    printa_arvore(root->irmao, prof);
+}
+
+void salva_arquivos(Arquivo* arq, FILE* file){
+    if (arq == NULL) {
+        int flag_filho = 0;
+        fwrite(&flag_filho, sizeof(int), 1, file);
+        return;
+    }
+
+    int flag_filho = 1;
+    fwrite(&flag_filho, sizeof(int), 1, file);
+
+    fwrite(arq, sizeof(Arquivo), 1, file);
+
+    // Recursively save child and sibling
+    salva_arquivos(arq->filho, file);
+    salva_arquivos(arq->irmao, file);
+}
+
+Arquivo* carrega_arquivos(FILE* file){
+    Arquivo* arq_auxiliar;
+
+    if(fread(&arq_auxiliar, sizeof(Arquivo), 1, file) != 1)
+        return NULL;
+
+    Arquivo *arq = (Arquivo *)malloc(sizeof(Arquivo));
+    fread(arq, sizeof(Arquivo), 1, file);
+
+    arq->filho = carrega_arquivos(file);
+    arq->irmao = carrega_arquivos(file);
+
+    return arq;
+}
+
+void salva_sistema_de_arquivos(Sistema_de_arquivos *fs, const char *arquivo){
+    FILE* file = fopen(arquivo, "wb");
+
+    if(file == NULL){
+        fprintf(stderr,"fopen :(\n");
+        return;
+    }
+
+    fwrite(fs->fat, sizeof(int), MAX_BLOCKS, file);
+    fwrite(fs->bitmap, sizeof(int), MAX_BLOCKS, file);
+
+    salva_arquivos(fs->root, file);
+    fclose(file);
+}
+
+Sistema_de_arquivos* inicia_sistema_de_arquivos(const char* arquivo){
+    Sistema_de_arquivos* fs = (Sistema_de_arquivos*)malloc(sizeof(Sistema_de_arquivos));
+    
+    fs->root = (Arquivo*)malloc(sizeof(Arquivo)); 
+    strcpy(fs->root->nome, "/");
+    fs->root->tamanho_bytes = 0;
+    fs->root->tempo_criacao = time(NULL);
+    fs->root->tempo_ult_acess = fs->root->tempo_criacao;
+    fs->root->tempo_ult_mod = fs->root->tempo_criacao;
+    fs->root->root = 1;
+    fs->root->ehDir = 1;
+    fs->root->irmao = NULL;
+    fs->root->filho = NULL;
+
+    for(int i=0; i<MAX_BLOCKS; i++){
+        fs->fat[i] = -1;
+        fs->bitmap[i] = 0;
+    }
+
+    fs->freeSpace = MAX_BLOCKS * BLOCK_SIZE;
+
+    salva_sistema_de_arquivos(fs, arquivo);
+
+    return fs;
+}
+
+Sistema_de_arquivos* carrega_sistema_de_arquivos(const char* arquivo){
+    FILE *file;
+
+    file = fopen(arquivo, "rb");
+
+    if(file == NULL)
+        return NULL;
+
+    Sistema_de_arquivos *fs = (Sistema_de_arquivos*)malloc(sizeof(Sistema_de_arquivos));
+
+    fread(fs->fat, sizeof(int), MAX_BLOCKS, file);
+    fread(fs->bitmap, sizeof(int), MAX_BLOCKS, file);
+
+    fs->root = carrega_arquivos(file);
+    fclose(file);
+    return fs;
+
+}
+
+void monta(const char* arquivo){
+    Sistema_de_arquivos* fs = carrega_sistema_de_arquivos(arquivo);
+
+    if(fs == NULL){
+        fs = inicia_sistema_de_arquivos(arquivo);
+        printf("Sistema de arquivos %s criado.\n", arquivo);
+    }
+    else
+        printf("Sistema de arquivos montado a partir do arquivo %s.\n", arquivo);
+
+    printa_arvore(fs->root, 0);
+}
+
+void copia(const char* origem, const char* destino){
     
 }
 
-void copia(char* origem, char* destino){
+void criadir(const char* diretorio){
     
 }
 
-void criadir(char* diretorio){
+void apagadir(const char* diretorio){
     
 }
 
-void apagadir(char* diretorio){
+void mostra(const char* arquivo){
     
 }
 
-void mostra(char* arquivo){
+void toca(const char* arquivo){
     
 }
 
-void toca(char* arquivo){
+void apaga(const char* arquivo){
     
 }
 
-void apaga(char* arquivo){
-    
-}
-
-void lista(char* diretorio){
+void lista(const char* diretorio){
     
 }
 
@@ -51,7 +156,7 @@ void atualizadb(){
     
 }
 
-void busca(char* string){
+void busca(const char* string){
     
 }
 
@@ -73,7 +178,9 @@ void main_loop(){
     /* Initialize history */
     using_history();  
 
-    while((line_read = readline("{ep3}: "))){
+    char prompt[8] = "{ep3}: ";
+
+    while((line_read = readline(prompt))){
 
         add_history(line_read);
 
@@ -82,50 +189,50 @@ void main_loop(){
         /* Comandos internos */
         if(!strcmp(firstArg, "monta")){
             
-            char* arquivo = strtok(line_read, " ");
+            char* arquivo = strtok(NULL, " ");
             monta(arquivo);
 
         }
         else if(!strcmp(firstArg, "copia")){
 
-            char* origem = strtok(line_read, " ");
-            char* destino = strtok(line_read, " ");
+            char* origem = strtok(NULL, " ");
+            char* destino = strtok(NULL, " ");
             copia(origem, destino);
         
         }
         else if(!strcmp(firstArg, "criadir")){
 
-            char* diretorio = strtok(line_read, " ");
+            char* diretorio = strtok(NULL, " ");
             criadir(diretorio);
 
         }
         else if(!strcmp(firstArg, "apagadir")){
 
-            char* diretorio = strtok(line_read, " ");
+            char* diretorio = strtok(NULL, " ");
             apagadir(diretorio);
 
         }
         else if(!strcmp(firstArg, "mostra")){
 
-            char* arquivo = strtok(line_read, " ");
+            char* arquivo = strtok(NULL, " ");
             mostra(arquivo);
 
         }
         else if(!strcmp(firstArg, "toca")){
 
-            char* arquivo = strtok(line_read, " ");
-            toca (arquivo);
+            char* arquivo = strtok(NULL, " ");
+            toca(arquivo);
         
         }
         else if(!strcmp(firstArg, "apaga")){
 
-            char* arquivo = strtok(line_read, " ");
+            char* arquivo = strtok(NULL, " ");
             apaga(arquivo);
 
         }
         else if(!strcmp(firstArg, "lista")){
 
-            char* diretorio = strtok(line_read, " ");
+            char* diretorio = strtok(NULL, " ");
             lista(diretorio);
 
         }
@@ -136,7 +243,7 @@ void main_loop(){
         }
         else if(!strcmp(firstArg, "busca")){
 
-            char* str = strtok(line_read, " ");
+            char* str = strtok(NULL, " ");
             busca(str);
 
         }
--- operating-systems-mac0422/EP3/ep3.h
+++ operating-systems-mac0422/EP3/ep3.h
@@ -1,32 +1,60 @@
-/* FUNÇÕES DO SHELL*/
+/* Bibliotecas */
+#include <stdio.h>
+#include <stdlib.h> 
+#include <time.h>
 
-/* FUNÇÕES BASE */
+/* Defines */
+#define MAX_NAME 256
+#define BLOCK_SIZE 4096  // 4KB
+#define MAX_BLOCKS 25600 // 100MB / 4KB
+
+/* FUNÇÕES DO SHELL*/
 /* Loop principal de leitura de comandos enviados pelo usuário*/
 void main_loop();
 
+/* Estruturas de dados utilizadas */
+
+/* Estrutura de um arquivo, pode ser arquivo regular ou diretório*/
+typedef struct Arquivo{
+    char nome[MAX_NAME];
+    int tamanho_bytes;
+    time_t tempo_criacao;
+    time_t tempo_ult_mod;
+    time_t tempo_ult_acess;
+    struct Arquivo* filho;
+    struct Arquivo* irmao;
+    int ehDir;
+    int root;
+}Arquivo;
+
+/* Estrutura do sistema de arquivos */
+typedef struct {
+    Arquivo* root;
+    int fat[MAX_BLOCKS];
+    int bitmap[MAX_BLOCKS];
+    size_t freeSpace;
+}Sistema_de_arquivos;
+
+
+/* FUNÇÕES PRINCIPAIS */
+void printa_arvore(Arquivo* root, int prof);
+Sistema_de_arquivos* inicia_sistema_de_arquivos(const char* arquivo);
+Sistema_de_arquivos* carrega_sistema_de_arquivos(const char* arquivo);
+void salva_sistema_de_arquivos(Sistema_de_arquivos *fs, const char *arquivo);
+void salva_arquivos(Arquivo* arq, FILE* file);
+Arquivo* carrega_arquivos(FILE* file);
+
 /* COMANDOS DE ARQUIVO */
-void monta(char* arquivo);
-
-void copia(char* origem, char* destino);
-
-void criadir(char* diretorio);
-
-void apagadir(char* diretorio);
-
-void mostra(char* arquivo);
-
-void toca(char* arquivo);
-
-void apaga(char* arquivo);
-
-void lista(char* diretorio);
-
+void monta(const char* arquivo);
+void copia(const char* origem,const char* destino);
+void criadir(const char* diretorio);
+void apagadir(const char* diretorio);
+void mostra(const char* arquivo);
+void toca(const char* arquivo);
+void apaga(const char* arquivo);
+void lista(const char* diretorio);
 void atualizadb();
-
-void busca(char* string);
-
+void busca(const char* string);
 void status();
-
 void desmonta();
-
 void sai();
